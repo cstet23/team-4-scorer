75a28c2d6fc5e1199f4cf18304704bb2fa26f024
aidant <aidant@udel.edu>
2022-03-22 15:07:09 -0400

    added a Counter component

---
+++ src/Counter.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export const Counter = ({ label }: { label: string }) => {
+    const [count, setCount] = useState<number>(0);
+
+    return (
+        <div style={{ display: "flex", gap: "1rem" }}>
+            <h3>{label}</h3>
+            <Button
+                onClick={() => {
+                    setCount(count + 1);
+                }}
+            >
+                -
+            </Button>
+            <Button
+                onClick={() => {
+                    setCount(count - 1);
+                }}
+            >
+                +
+            </Button>
+            <p>{count}</p>
+        </div>
+    );
+};
